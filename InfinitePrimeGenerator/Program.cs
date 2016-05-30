5017428dc032454e69d5d4192590eeddfeaed6d5
infomaven <whitfld64@hotmail.com>
2016-05-29 21:06:35 -0700

    Updated square root test to break if any existing primes are > squareroot of testPrime.

--- InfinitePrimeGenerator/Program.cs
+++ InfinitePrimeGenerator/Program.cs
@@ -12,17 +12,15 @@ namespace InfinitePrimeGenerator
 
 		public static void Main (string[] args)
 		{
-
 			Stopwatch stopwatch = new Stopwatch ();
 			stopwatch.Start ();
 			TimeSpan ts;
+
 			foreach (double p in AllPrimes()) {
 				ts = stopwatch.Elapsed;
 				string elapsedTime = String.Format ("{0:0000}:{1:0000}.{2:00000000000000}",
 					                     ts.Minutes, ts.Seconds, ts.Milliseconds);
 				if ( ts.Minutes == 1 ) {
-				/// for local testing
-//				if (ts.Seconds == 1) {
 				    Console.WriteLine ("Max Prime = " + p);
 
 					break;
@@ -35,9 +33,24 @@ namespace InfinitePrimeGenerator
 			stopwatch.Stop();
 		}
 
+		private bool isPrime (int testVal, int comparisonVal )
+		{
+		    bool result = false;
+		    if ( (testVal % testVal == 0) && (testVal % 1 == 0) && ( testVal % comparisonVal != 1) ) {
+		         result = true; 
+		         }
+		    return result;
+
+		}
+
+		/// <summary>
+		/// Method that generates a list of Primes using odd number testPrimes. First two primes are front loaded. 
+		/// </summary>
+		/// <returns>The primes.</returns>
 		private static IEnumerable<double> AllPrimes ()
 		{
 			List<double> primesThusFar = new List<double> ();
+			List<double> primesExcluded = new List<double> ();
 			primesThusFar.Add (2);
 			yield return 2;
 			primesThusFar.Add (3);
@@ -46,28 +59,31 @@ namespace InfinitePrimeGenerator
 			double testPrime = 5;
 			double testSqRoot;
 			while (true) {
-				bool isPrime = true;
-
+			    
+				bool isPrime = true;			
 				foreach (double n in primesThusFar) {
-					/// firstCut: eliminate testPrime if it can be factored by anything from our list
+					/// secondCut: eliminate testPrime if it can be factored by anything from our list
 //					if (testPrime % n == 0) {
 //						isPrime = false;
 //						break;		            				
 //					}
 
-					/// secondCut: eliminate testPrime if it's square root can be factored by anything from our list
-				    testSqRoot = Math.Sqrt (testPrime);
+					/// thirdCut: eliminate testPrime if it's square root can be factored by anything from our list
+					testSqRoot = Math.Sqrt (testPrime);
+					if ( testSqRoot < n )
+						break;
 					if ( testSqRoot % n == 0 ) {
 						isPrime = false;
 						break;
 					}
                    
 				}
+				//// if none of previous tests eliminated testPrime, it can be added
 				if (isPrime) {
 					primesThusFar.Add (testPrime);
 					yield return testPrime;
 				}
-				/// make sure we always get an odd number
+				/// make sure testPrime is always an odd number
 				testPrime += 2;
 			}
 		}
